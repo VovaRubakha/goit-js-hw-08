4db18594ac08ab218aeca9ddd3ba6af331efbb5b
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-04-25 12:59:28 +0300

    on review task 2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,21 @@
 import Player from '@vimeo/player';
 
-const iframe = document.querySelector('#vimeo-player');
-console.log(iframe);
+import throttle from 'lodash.throttle';
 
-const player = new Player(iframe);
+const KEY_LS = `video-current-time`;
+const player = new Player('vimeo-player');
+
+const parsedData = JSON.parse(localStorage.getItem(KEY_LS));
+const prevTime = parsedData ? parsedData.seconds : 0;
+
+player.setCurrentTime(prevTime);
+function getCurrentTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(KEY_LS, JSON.stringify({ seconds: currentTime }));
+  console.log(`${currentTime}`);
+}
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
 player.on('play', function () {
   console.log('played the video!');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
 
+const KEY_LS_INPUT = `feedback-form-state`;
+const form = document.querySelector(`.feedback-form`);
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(KEY_LS_INPUT)));
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+const saveData = {};
+function inputForm(e) {
+  let { name, value } = e.target;
+  saveData[name] = value;
+  localStorage.setItem(KEY_LS_INPUT, JSON.stringify(saveData));
+}
+
+form.addEventListener(`submit`, submitForm);
+form.addEventListener(`input`, throttle(inputForm, 500));
